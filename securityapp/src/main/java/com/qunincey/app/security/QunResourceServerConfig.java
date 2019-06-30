a6c78a6fdc3cbe787a675756a1c872067a756a7e
qunincey <408212480@qq.com>
2019-06-30 14:42:55 +0800

    OAuth2授权

---
+++ securityapp/src/main/java/com/qunincey/app/security/QunResourceServerConfig.java
@@ -0,0 +1,69 @@
+package com.qunincey.app.security;
+
+import com.qunincey.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.qunincey.security.core.properties.SecurityConstants;
+import com.qunincey.security.core.properties.SecurityProperties;
+import com.qunincey.security.core.validate.code.ValidateCodeSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
+
+/**
+ * @program: spingsecurity
+ * @description:
+ * @author: qiuxu
+ * @create: 2019-06-29 20:25
+ **/
+@Configuration
+@EnableResourceServer
+public class QunResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationSuccessHandler qunAuthentionSuccessHandler;
+
+    @Autowired
+    private AuthenticationFailureHandler qunAuthentionFailHandler;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer qunSocialSecurityConfig;
+
+    @Autowired
+    public SecurityProperties securityProperties;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_SIGN_IN_PROCESSING_URL_FORM)
+                .successHandler(qunAuthentionSuccessHandler)
+                .failureHandler(qunAuthentionFailHandler);
+        http//.apply(validateCodeSecurityConfig)
+                //.and()
+                .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                .apply(qunSocialSecurityConfig)
+                .and()
+                .authorizeRequests()
+                .antMatchers(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,securityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*","/auth/*",securityProperties.getBrowser().getSignUpUrl(),"/user/regist",SecurityConstants.DEFAULT_SESSION_INVALID_URL,securityProperties.getBrowser().getSignOutUrl())
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                .and()
+                .csrf().disable();
+
+//        authorizeConfigManager.config(http.authorizeRequests());
+    }
+}
--- securityapp/src/main/java/com/qunincey/app/security/authentication/QunAuthentionSuccessHandler.java
+++ securityapp/src/main/java/com/qunincey/app/security/authentication/QunAuthentionSuccessHandler.java
@@ -1,12 +1,22 @@
 package com.qunincey.app.security.authentication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mysql.fabric.xmlrpc.Client;
 import com.qunincey.security.core.properties.LoginType;
 import com.qunincey.security.core.properties.SecurityProperties;
+import com.qunincey.security.core.support.SimpleResponse;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +24,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Base64;
 
 /**
  * @program: spingsecurity
@@ -32,17 +43,73 @@ public class QunAuthentionSuccessHandler extends SavedRequestAwareAuthentication
     @Autowired
     private SecurityProperties securityProperties;
 
+    private String credentialsCharset = "UTF-8";
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
         logger.info("登录成功");
 
-        if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())){
-            httpServletResponse.setContentType("application/json,charset-UTF-8");
-            httpServletResponse.getWriter().write(objectMapper.writeValueAsString(authentication));
-        }else {
-            super.onAuthenticationSuccess(httpServletRequest,httpServletResponse,authentication);
+
+        String header = httpServletRequest.getHeader("Authorization");
+        if (header == null || !header.toLowerCase().startsWith("basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
         }
 
 
+        String[] tokens = this.extractAndDecodeHeader(header, httpServletRequest);
+
+        assert tokens.length == 2;
+
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+        if (clientDetails == null){
+            throw new UnapprovedClientAuthenticationException("clientId对应的配置不存在"+clientId);
+        }else if (!StringUtils.equals(clientDetails.getClientSecret(),clientSecret)){
+            throw new UnapprovedClientAuthenticationException("clientSecret不正确"+clientSecret);
+        }
+
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP,clientId,clientDetails.getScope(),"custom");
+
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request,authentication);
+
+        OAuth2AccessToken token=authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        httpServletResponse.setContentType("application/json,charset-UTF-8");
+        httpServletResponse.getWriter().write(objectMapper.writeValueAsString(token));
+
+    }
+
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request) throws IOException {
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+
+        byte[] decoded;
+        try {
+            decoded = Base64.getDecoder().decode(base64Token);
+        } catch (IllegalArgumentException var7) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, this.getCredentialsCharset(request));
+        int delim = token.indexOf(":");
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        } else {
+            return new String[]{token.substring(0, delim), token.substring(delim + 1)};
+        }
+    }
+
+    protected String getCredentialsCharset(HttpServletRequest httpRequest) {
+        return this.credentialsCharset;
     }
 }
